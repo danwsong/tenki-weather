ab9a625236aa250f188f82449dd8ec123c397b4b
Daniel Song <servthedev@gmail.com>
2016-03-28 10:40:14 -0400

    Initial Commit

---
+++ Tenki WatchKit Extension/GlanceController.swift
@@ -0,0 +1,31 @@
+//
+//  GlanceController.swift
+//  Tenki WatchKit Extension
+//
+//  Created by Daniel Song on 2016-03-28.
+//  Copyright © 2016 Daniel Song. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class GlanceController: WKInterfaceController {
+
+    override func awakeWithContext(context: AnyObject?) {
+        super.awakeWithContext(context)
+        
+        // Configure interface objects here.
+    }
+
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
---
+++ Tenki/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Tenki
+//
+//  Created by Daniel Song on 2016-03-28.
+//  Copyright © 2016 Daniel Song. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
